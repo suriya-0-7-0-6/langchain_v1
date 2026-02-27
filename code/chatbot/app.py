4f0962edd355456c4142b4b27a217dee5083b6a4
suriya <phoenixova01@gmail.com>
2026-02-27 15:10:02 +0530

    Blah Blah

---
+++ code/chatbot/app.py
@@ -0,0 +1,27 @@
+from langchain_ollama import ChatOllama
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+import streamlit as st
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
+os.environ["LANGSMITH_API_KEY"] = os.getenv("LANGSMITH_API_KEY")
+os.environ["LANGSMITH_TRACING_V2"] = os.getenv("LANGSMITH_TRACING_V2")
+os.environ["LANGSMITH_TRACING"] = os.getenv("LANGSMITH_TRACING")
+os.environ["LANGSMITH_ENDPOINT"] = os.getenv("LANGSMITH_ENDPOINT")
+os.environ["LANGSMITH_PROJECT"] = os.getenv("LANGSMITH_PROJECT")
+
+llm = ChatOllama(model="qwen2.5:latest")
+
+prompt = ChatPromptTemplate.from_messages([
+    ("system", "You are a helpful assistant. Please response to the user queries"),
+    ("user", "Question: {question}")
+])
+
+st.title("Langchain Demo with Ollama")
+
+
